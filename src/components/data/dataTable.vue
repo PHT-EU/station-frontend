ec0f5338c1859c79987ff60b61bc869c26c9470d
davidhieber <david.hieber@uni-tuebingen.de>
2021-08-17 17:00:07 +0200

    added extra colums and footer

---
+++ src/components/data/dataTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <table class="table is-fullwidth is-hoverable ">
+    <thead>
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>storage type</th>
+      <th>created at</th>
+      <th>access path</th>
+      <th>proposal id</th>
+      <th>items</th>
+      <th>target field</th>
+      <th>fhir user</th>
+      <th>fhir password</th>
+      <th>fhir server type</th>
+      <th>Delete/Add</th>
+    </tr>
+    </thead>
+    <tfoot>
+    <tr>
+      <th>id</th>
+      <th><input class="input is-primary" type="text" placeholder="name"></th>
+      <th><input class="input is-primary" type="text" placeholder="storage type"></th>
+      <th>created at</th>
+      <th><input class="input is-primary" type="text" placeholder="access path"></th>
+      <th><input class="input is-primary" type="text" placeholder="proposal id"></th>
+      <th>n_items</th>
+      <th><input class="input is-primary" type="text" placeholder="target field"></th>
+      <th><input class="input is-primary" type="text" placeholder="fhir user"></th>
+      <th><input class="input is-primary" type="password" placeholder="fhir password"></th>
+      <th><input class="input is-primary" type="text" placeholder="fhir server type"></th>
+      <th>
+        <button class="button is-success is-outlined" v-on:click="addDataset()">
+          <i class="far fa-plus-square"></i>
+        </button>
+      </th>
+    </tr>
+    </tfoot>
+    <tbody>
+    <tr v-for="row in tableData" :key="row.id">
+      <td>{{row.id}}</td>
+      <td>{{ row.name }}</td>
+      <td>{{ row.storage_type }}</td>
+      <td>{{ row.created_at }}</td>
+      <td>{{ row.access_path }}</td>
+      <td>{{ row.proposal_id }}</td>
+      <td>{{row.n_items}}</td>
+      <td>{{row.target_field}}</td>
+      <td>{{row.fhir_user}}</td>
+      <td class="hidetext">{{row.fhir_password}}</td>
+      <td>{{row.fhir_server_type}}</td>
+      <td>
+        <button class="button is-danger is-outlined "  v-on:click="deleteDataset(row.id)">
+          <i class="far fa-trash-alt"></i>
+        </button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+export default {
+  name: "dataTable",
+  props: {  tableData: Array},
+
+}
+</script>
+
+<style scoped>
+.hidetext { -webkit-text-security: disc; /* Default */ }
+</style>
--- src/views/Data.vue
+++ src/views/Data.vue
@@ -8,17 +8,51 @@
             <tr>
               <th>id</th>
               <th>name</th>
-              <th>created at</th>
               <th>storage type</th>
-              <th>delete dataset</th>
+              <th>created at</th>
+              <th>access path</th>
+              <th>proposal id</th>
+              <th>items</th>
+              <th>target field</th>
+              <th>fhir user</th>
+              <th>fhir password</th>
+              <th>fhir server type</th>
+              <th>Delete/Add</th>
             </tr>
            </thead>
+          <tfoot>
+          <tr>
+            <th>id</th>
+            <th><input class="input is-primary" type="text" placeholder="name"></th>
+            <th><input class="input is-primary" type="text" placeholder="storage type"></th>
+            <th>created at</th>
+            <th><input class="input is-primary" type="text" placeholder="access path"></th>
+            <th><input class="input is-primary" type="text" placeholder="proposal id"></th>
+            <th>n_items</th>
+            <th><input class="input is-primary" type="text" placeholder="target field"></th>
+            <th><input class="input is-primary" type="text" placeholder="fhir user"></th>
+            <th><input class="input is-primary" type="password" placeholder="fhir password"></th>
+            <th><input class="input is-primary" type="text" placeholder="fhir server type"></th>
+            <th>
+              <button class="button is-success is-outlined" v-on:click="addDataset()">
+                <i class="far fa-plus-square"></i>
+              </button>
+            </th>
+          </tr>
+          </tfoot>
           <tbody>
             <tr v-for="row in tableData" :key="row.id">
               <td>{{row.id}}</td>
               <td>{{ row.name }}</td>
-              <td>{{ row.created_at }}</td>
               <td>{{ row.storage_type }}</td>
+              <td>{{ row.created_at }}</td>
+              <td>{{ row.access_path }}</td>
+              <td>{{ row.proposal_id }}</td>
+              <td>{{row.n_items}}</td>
+              <td>{{row.target_field}}</td>
+              <td>{{row.fhir_user}}</td>
+              <td class="hidetext">{{row.fhir_password}}</td>
+              <td>{{row.fhir_server_type}}</td>
               <td>
                 <button class="button is-danger is-outlined "  v-on:click="deleteDataset(row.id)">
                   <i class="far fa-trash-alt"></i>
@@ -27,19 +61,25 @@
             </tr>
           </tbody>
           </table>
-        <button class="button is-success is-outlined" v-on:click="addDataset()">
-          <i class="far fa-plus-square"></i>
-        </button>
       </div>
+
+      <PopUpForm/>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import PopUpForm from '@/components/data/PopUpForm';
 
 export default {
   name: "Data",
+  components: "dataTable",
+  setup() {
+     return {
+        PopUpForm
+     }
+  },
   data() {
     return  {
       dataSets: [],
@@ -82,6 +122,14 @@ export default {
           dataSetDict.name = this.dataSets[dataSet].name;
           dataSetDict.created_at = this.dataSets[dataSet].created_at;
           dataSetDict.storage_type = this.dataSets[dataSet].storage_type;
+          dataSetDict.data_type = this.dataSets[dataSet].data_type;
+          dataSetDict.proposal_id = this.dataSets[dataSet].proposal_id;
+          dataSetDict.access_path = this.dataSets[dataSet].access_path;
+          dataSetDict.n_items = this.dataSets[dataSet].n_items;
+          dataSetDict.target_field = this.dataSets[dataSet].target_field;
+          dataSetDict.fhir_user = this.dataSets[dataSet].fhir_user;
+          dataSetDict.fhir_password = this.dataSets[dataSet].fhir_password;
+          dataSetDict.fhir_server_type = this.dataSets[dataSet].fhir_server_type;
           this.tableData.push(dataSetDict);
         }
         console.log(this.tableData)
@@ -98,5 +146,5 @@ export default {
 </script>
 
 <style scoped>
-
+.hidetext { -webkit-text-security: disc; /* Default */ }
 </style>
