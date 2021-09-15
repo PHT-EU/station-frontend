1ab753072572845e81ec12bd4432fd3828bcc05f
davidhieber <david.hieber@uni-tuebingen.de>
2021-09-15 13:10:03 +0200

    added elemet to view the uplodoet code

--- src/components/localTrains/showEndpointCode.vue
+++ src/components/localTrains/showEndpointCode.vue
@@ -1,11 +1,49 @@
 <template>
-  code
-  code
+  <div class="line-numbers">
+    <pre :class="language_class">
+      <code>{{endpoint_code}}
+      </code>
+    </pre>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
+import 'prismjs/components/prism-python'
+import 'prismjs/components/prism-r'
+import 'prismjs/components/prism-json'
+import 'prismjs/plugins/line-numbers/prism-line-numbers'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 export default {
-  name: "showEndpoints"
+  name: "showEndpoints",
+  data(){
+    return {
+      endpoint_code: "",
+      lang: "python"
+    }
+  },
+  methods:{
+    loadFileNames(){
+      let url = `${process.env.VUE_APP_STATION_API}/local_trains/get_endpoint`;
+      axios.get(url).then(response => {
+        this.endpoint_code = response.data;
+      });
+    },
+  },
+  computed: {
+    language_class() {
+      return`language-${this.lang}`
+    },
+
+  },
+  mounted() {
+    this.loadFileNames()
+    Prism.highlightAll()
+  }
+
+
 }
 </script>
 
--- src/views/LocalTrain.vue
+++ src/views/LocalTrain.vue
@@ -1,13 +1,15 @@
 <template>
 Local Train
   <upload-endpoint></upload-endpoint>
+  <show-endpoint-code></show-endpoint-code>
 </template>
 
 <script>
 import uploadEndpoint from "@/components/localTrains/uploadEndpoint";
+import showEndpointCode from "@/components/localTrains/showEndpointCode";
 export default {
   name: "LocalTrain",
-  components: {uploadEndpoint}
+  components: {uploadEndpoint, showEndpointCode}
 }
 </script>
 
