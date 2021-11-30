7838fb7f60f916aeacde048f5935f8cd50950399
migraf <michael.graf3110@gmail.com>
2021-11-30 08:03:06 +0100

    use station env for api address

--- src/api/dockerTrainsApi.js
+++ src/api/dockerTrainsApi.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
+import { env } from '@/env';
 
 export async function getDockerTrainConfigs() {
-    const url = `${process.env.VUE_APP_STATION_API}/trains/docker/configs/all`;
+    const url = `${env.API_ADDRESS}/trains/docker/configs/all`;
 
     return axios.get(url).then((response) => response.data);
 }
 
 export async function updateDockerTrainConfig(config) {
     console.log(config);
-    const url = `${process.env.VUE_APP_STATION_API}/trains/docker/config/${config.id}`;
+    const url = `${env.API_ADDRESS}/trains/docker/config/${config.id}`;
 
     return axios.put(url, config).then((response) => response.data);
 }
 
 export async function createDockerTrainConfig(config) {
     console.log(JSON.stringify(config));
-    const url = `${process.env.VUE_APP_STATION_API}/trains/docker/config`;
+    const url = `${env.API_ADDRESS}/trains/docker/config`;
 
     return axios.post(url, config).then((response) => response.data).catch((reason) => console.log(reason));
 }
